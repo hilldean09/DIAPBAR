e32ab46ca47e662f344c1c7231a94201ec85eeda
Dean Hill <hilldean09@gmail.com>
2025-11-16 09:57:07 +0800

    Define pure virtual base class

--- src/diapbar_core.hpp
+++ src/diapbar_core.hpp
@@ -21,6 +21,30 @@ namespace Diapbar::Core {
 
   constexpr BarFormat DEFAULT_BAR_FORMAT = BarFormat{ '[', '#', ' ', ']' };
 
+
+  template <typename NumT>
+  class Diapbar_Base {
+    public:
+
+    // Initialiseers //
+      virtual void initialise( int barLength ) = 0;
+      virtual void initialise( NumT* trackerPtr, NumT goal ) = 0;
+      virtual void initialise( NumT* trackerPtr, NumT goal, int barLength ) = 0;
+
+    // Accessors //
+      virtual char* getStringCache() const = 0;
+
+      virtual int getDisplayLength() const = 0;
+      virtual int getCacheLength() const = 0;
+
+    // Mutators //
+      virtual void setBarFormat( char* formatPtr ) = 0;
+
+      virtual char* buildBarToCache() const = 0;
+
+
+  };
 }
 
+}
 #endif
